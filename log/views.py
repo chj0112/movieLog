911c33ac8ee28d7997ce5591291db63b88655afa
chj0112 <chohj000112@gmail.com>
2023-06-21 00:52:25 +0900

    log/convert to class based view

--- log/views.py
+++ log/views.py
@@ -1,24 +1,32 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    return render(
-        request,
-        'log/index.html',
-        {
-            'posts': posts,
-        }
-    )
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(
+#         request,
+#         'log/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
 
-    return render(
-        request,
-        'log/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+class PostDetail(DetailView):
+    model = Post
+
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'log/single_post_page.html',
+#         {
+#             'post': post,
+#         }
+#     )
